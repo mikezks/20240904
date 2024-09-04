ec7388392862e3d10466cbd3fa3ecf094c40f8ac
mikezks <23002661+mikezks@users.noreply.github.com>
2024-09-04 15:45:32 +0200

    feat: search result, http

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight } from '../model/flight';
 
 @Component({
   selector: 'app-flight-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    DatePipe
+  ],
   templateUrl: './flight-search.component.html',
   styleUrl: './flight-search.component.scss'
 })
 export class FlightSearchComponent {
+  private http = inject(HttpClient);
+
   from = 'Hamburg';
   to = 'Graz';
   flights: Flight[] = [];
   selectedFlight: Flight | undefined;
 
   search(): void {
-    console.log(this.from, this.to);
+    const url = 'https://demo.angulararchitects.io/api/flight';
+    const params = { from: this.from, to: this.to };
+    const headers = { 'Accept': 'application/json' };
+
+    this.http.get<Flight[]>(url, { params, headers })
+      .subscribe(
+        flights => this.flights = flights
+      );
   }
 
   select(flight: Flight): void {
-    this.selectedFlight = flight;
+    this.selectedFlight = flight === this.selectedFlight
+      ? undefined
+      : flight;
   }
 }
