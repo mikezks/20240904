61d8509e87722c77d8efd8e8d62ff316cf0fa94c
mikezks <23002661+mikezks@users.noreply.github.com>
2024-09-05 11:25:47 +0200

    feat: base url token

--- src/app/flight-search/default-flight.service.ts
+++ src/app/flight-search/default-flight.service.ts
@@ -3,14 +3,16 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Flight } from '../model/flight';
 import { FlightService } from './flight.service';
+import { BASE_URL } from '../app.tokens';
 
 
 @Injectable()
 export class DefaultFlightService implements FlightService {
   private http = inject(HttpClient);
+  private baseUrl = inject(BASE_URL);
 
   find(from: string, to: string): Observable<Flight[]> {
-    const url = 'https://demo.angulararchitects.io/api/flight';
+    const url = this.baseUrl + '/flight';
     const params = { from, to };
     const headers = { 'Accept': 'application/json' };
 
