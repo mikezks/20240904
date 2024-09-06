a752a70cd390ddab61f7c69e9b156af45bd8ee90
mikezks <23002661+mikezks@users.noreply.github.com>
2024-09-06 17:10:19 +0200

    test: simple unit test flight search

--- src/app/booking/features/flight-search/flight-search.component.spec.ts
+++ src/app/booking/features/flight-search/flight-search.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FlightSearchComponent } from './flight-search.component';
+import { provideHttpClient } from '@angular/common/http';
+import { FlightService } from '../../data-access/flight.service';
+import { DummyFlightService } from '../../data-access/dummy-flight.service';
 
 describe('FlightSearchComponent', () => {
   let component: FlightSearchComponent;
@@ -8,10 +11,17 @@ describe('FlightSearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FlightSearchComponent]
+      imports: [FlightSearchComponent],
+      providers: [
+        provideHttpClient(),
+        {
+          provide: FlightService,
+          useClass: DummyFlightService
+        }
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(FlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
